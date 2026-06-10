269ed595f5fc16a6982ef746916b674a32d89ec5
lowsusie0500 <lowsusie0500@gmail.com>
2026-06-10 17:04:04 +0800

    Create Home component for CleanPro Manager

---
+++ page/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>CleanPro Manager</h1>
+      <p>System is running successfully 🚀</p>
+    </div>
+  );
+}
